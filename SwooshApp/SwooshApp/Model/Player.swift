c20a80d00bc35d89c9bad468f300f1f993086959
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-08 16:42:24 -0800

    Added small changes

---
+++ SwooshApp/SwooshApp/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  SwooshApp
+//
+//  Created by Gurjit Singh on 2/8/18.
+//  Copyright © 2018 Gurjit Singh. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
